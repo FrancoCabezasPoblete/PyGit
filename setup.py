1aa82c1c115f4219a825f38b3c3806221549d8f2
Frank <francoale2010@gmail.com>
2023-12-12 15:39:57 -0300

    chore: Added  Dependences

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='PyGit',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        "GitPython==3.1.40"
+    ],
     entry_points={
         'console_scripts': [
             'pygit = src.main:main',
